7bb8a5d7d8ccfbb6b4789370c812e9d03b4fec0a
Maheen Israr <maheenshy.1702@gmail.com>
2024-08-06 12:54:21 +0200

    Passing an argument

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+def my_function(lname):
+    print ('hello' , lname)
+
+my_function(World)
+my_function("Universe")
+
+#as simple as it gets, when you pass an argument with + sign, it will concatenate together , and the concatenation must be of same data type like if there is str defined for one variable then the other should also be str. But when we put ´,´ then it doesnt matter if the arguments that are passed are alike or not.
+
